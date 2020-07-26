wise-king-sullyman/Calculator

commit 4881fe7d0dd5d2af9021d23f8adcdbd4e0e150a2

Fixed bug related to display overflow

index.html
@@ -34,9 +34,9 @@
                 <button id="+" class="operator">+</button>
                 </div>
             <div id="column5" class="column">
-                <button id="Backspace">Back</button>
-                <button id="CLR">Clear<R/button>
-                <button id="equals">=</button>
+                <button id="Backspace" class="operator">Back</button>
+                <button id="CLR" class="operator">Clear<R/button>
+                <button id="equals" class="operator">=</button>
             </div>
         </div>
     </div>

script.js
@@ -8,6 +8,7 @@ let operatorKeys = ["+","-","/","*"]
 let operand1;
 let operand2;
 let operator;
+let lastCalcOverflow = 0;
 
 let display = document.querySelector('#display')
 
@@ -21,8 +22,9 @@ let checkForDecimal = (e) => {
 };
 
 let addToDisplay = (e) => {
+    let charsInDisplay = display.textContent.length;
     if(checkForDecimal(e)){return;}
-    if(display.textContent.length > 12){return;}
+    if(charsInDisplay > 12){return;}
     display.textContent += e.target.textContent;
 };
 
@@ -35,17 +37,30 @@ let clearOps = () => {
 };
 
 let addOperator = (e) => {
+    if(lastCalcOverflow == 1) {
+        display.textContent = +operand1.toFixed(2)
+        lastCalcOverflow = 2;
+    };
+
     if(operator) {
         operand1 = operate() 
         display.textContent = operand1
-    }else {operand1 = display.textContent;}
+    }else {operand1 = display.textContent};
     addToDisplay(e)
     operator = e.target.id;
 };
 
 let operate = () => { 
     operand2 = display.textContent.slice(operand1.toString().length+1)
-    if(!operand2) {return operand1}
+    if(!operand2) {
+        return operand1
+    };
+
+    if(lastCalcOverflow && operator == "*") {
+        lastCalcOverflow = 0;
+        return ((Math.round(multiply(operand1,operand2))*100)/100)
+    };
+
     switch(operator) {
         case "+":
              return add(operand1,operand2);
@@ -69,9 +84,11 @@ let handleOperate = () => {
             clearOps()
             return
         }
-        if(operationResult.toString().length > 12){
-            display.textContent = operationResult.toFixed(12);
+        if(operationResult.toString().length > 10){
+            display.textContent = operationResult.toFixed(10);
+            lastCalcOverflow = 1;
             clearOps()
+            operand1 = operationResult;
             return
         }
         display.textContent = operationResult;
@@ -83,10 +100,19 @@ let itemInArray = (targetItem,array) => array.find(item => item == targetItem);
 
 let clickKey = id => document.getElementById(id).click();
 
-let logKey = (e) => {
+let addClass = e => e.target.classList.add("buttonClicked");
+
+let remClass = e => {
+    e.target.classList.remove("buttonClicked");
+};
+
+let logKey = e => {
     e.preventDefault();
     let key = e.key;
-    if(itemInArray(key,operandKeys)){clickKey(key)};
+    if(itemInArray(key,operandKeys)){
+        clickKey(key)
+
+    };
     if(itemInArray(key,operatorKeys)){clickKey(key)};
     if(key == 0){clickKey("0")}
     if(key == "Enter" || key == "="){clickKey("equals")};
@@ -99,6 +125,10 @@ document.addEventListener("keydown",logKey);
 let removeCharFromDisplay = () => {
     display.textContent = display.textContent.slice(0,-1);
 };
+
+let buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener("click",addClass))
+buttons.forEach(button => button.addEventListener("transitionend",remClass));
 
 let operands = document.querySelectorAll('.operand');
 operands.forEach(operand => operand.addEventListener("click",addToDisplay));

style.css
@@ -43,22 +43,25 @@ button {
     font-size: x-large;
     box-sizing: border-box;
     border: none;
-    transition: .1s
+    transition: .2s;
+    transition-property: background-color;
+
 }
 
 .operand {
     background-color: #62c7e2;
 }
 
-.operand:hover {
-    background-color:rgb(84, 150, 212);
-}
-
 button:hover {
-    background-color: #a1482b
+    border-width: 1px;
+    border-style: solid;
 }
 
 #equals {
     height: 50%;
+}
+
+.buttonClicked {
+    background-color: rgb(196, 196, 196);
 }
 

commit 8d018d6db3ffe435316880280a77c4925349d2e3

Fixed bugs introduced in 4881fe7 among others; slightly refined styling

index.html
@@ -34,9 +34,9 @@
                 <button id="+" class="operator">+</button>
                 </div>
             <div id="column5" class="column">
-                <button id="Backspace" class="operator">Back</button>
-                <button id="CLR" class="operator">Clear<R/button>
-                <button id="equals" class="operator">=</button>
+                <button id="Backspace" class="relation">Back</button>
+                <button id="CLR" class="relation">Clear<R/button>
+                <button id="equals" class="relation">=</button>
             </div>
         </div>
     </div>

script.js
@@ -9,6 +9,7 @@ let operand1;
 let operand2;
 let operator;
 let lastCalcOverflow = 0;
+let lastCalc;
 
 let display = document.querySelector('#display')
 
@@ -38,8 +39,8 @@ let clearOps = () => {
 
 let addOperator = (e) => {
     if(lastCalcOverflow == 1) {
-        display.textContent = +operand1.toFixed(2)
-        lastCalcOverflow = 2;
+        display.textContent = +lastCalc.toFixed(2)
+        lastCalcOverflow = 0;
     };
 
     if(operator) {
@@ -56,9 +57,9 @@ let operate = () => {
         return operand1
     };
 
-    if(lastCalcOverflow && operator == "*") {
-        lastCalcOverflow = 0;
-        return ((Math.round(multiply(operand1,operand2))*100)/100)
+    if(lastCalc){
+        operand1 = lastCalc;
+        lastCalc = ""
     };
 
     switch(operator) {
@@ -88,7 +89,7 @@ let handleOperate = () => {
             display.textContent = operationResult.toFixed(10);
             lastCalcOverflow = 1;
             clearOps()
-            operand1 = operationResult;
+            lastCalc = operationResult;
             return
         }
         display.textContent = operationResult;
@@ -123,12 +124,15 @@ let logKey = e => {
 document.addEventListener("keydown",logKey);
 
 let removeCharFromDisplay = () => {
-    display.textContent = display.textContent.slice(0,-1);
+    if(display.textContent){
+        display.textContent = display.textContent.slice(0,-1);
+    };
 };
 
 let buttons = document.querySelectorAll('button');
 buttons.forEach(button => button.addEventListener("click",addClass))
 buttons.forEach(button => button.addEventListener("transitionend",remClass));
+buttons.forEach(button => button.addEventListener("transitioncancel",remClass));
 
 let operands = document.querySelectorAll('.operand');
 operands.forEach(operand => operand.addEventListener("click",addToDisplay));

style.css
@@ -43,7 +43,7 @@ button {
     font-size: x-large;
     box-sizing: border-box;
     border: none;
-    transition: .2s;
+    transition: .1s;
     transition-property: background-color;
 
 }
@@ -52,9 +52,16 @@ button {
     background-color: #62c7e2;
 }
 
-button:hover {
-    border-width: 1px;
-    border-style: solid;
+.operand:hover {
+    background-color: #62c6e29f;
+}
+
+.operator:hover  {
+    background-color: #e27e6293;
+}
+
+.relation:hover {
+    background-color: #e27e6293;
 }
 
 #equals {
@@ -62,6 +69,6 @@ button:hover {
 }
 
 .buttonClicked {
-    background-color: rgb(196, 196, 196);
+    background-color: rgba(255, 255, 255, 0.514);
 }
 